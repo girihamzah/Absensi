1d9e339850dc13ff71c900e6e9a761480384d94d
Giri Hamzah <girihamzah45@gmail.com>
2022-04-06 22:30:49 +0700

    Perpustakaan

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,17 @@
+@extends('layout.master')
+@section('content')
+
+<section class="boxBanner">
+    <div class="boxImage"></div>
+
+    <div class="boxContent">
+        <div class="name">
+            Perpustakaan SMK WIKRAMA BOGOR
+        </div>
+
+        <div class="headline">
+            Ilmu yang Amaliah, Amal yang Ilmiah, Akhlakul Karimah
+        </div>
+    </div>
+</section>
+@endsection
